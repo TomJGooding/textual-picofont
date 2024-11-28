4ae1627831979199bcf61801bbd8c3d3deb70943
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-11-28 14:25:43 +0000

    feat: tidy demo layout

--- examples/demo.py
+++ examples/demo.py
@@ -9,16 +9,16 @@ from textual_picofont.renderable import PicoFont as PicoFontRenderable
 class PicoFontApp(App):
     def compose(self) -> ComposeResult:
         yield Static(
-            PicoFontRenderable(string.ascii_uppercase),
-        )
-        yield Static(
-            PicoFontRenderable(string.digits),
-        )
-        yield Static(
             PicoFontRenderable(" !\"#$%&'()*+,-./"),
         )
         yield Static(
-            PicoFontRenderable(":;<=>?@"),
+            PicoFontRenderable(string.digits + ":;<=>?"),
+        )
+        yield Static(
+            PicoFontRenderable("@" + string.ascii_uppercase[:15]),
+        )
+        yield Static(
+            PicoFontRenderable(string.ascii_uppercase[15:]),
         )
 
 
